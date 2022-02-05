d80d3737cf31c3134705a9a6d828f3a9914ffe90
vhphong <vhphong@gmail.com>
2022-02-04 20:36:41 -0500

    final

---
+++ AI Hoang/python/ps0/shopSmart.py
@@ -0,0 +1,54 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for 
+# educational purposes provided that (1) you do not distribute or publish 
+# solutions, (2) you retain this notice, and (3) you provide clear 
+# attribution to UC Berkeley, including a link to 
+# http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero 
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and 
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+
+import shop
+def qSort(items):        
+    if( len(items) <= 1 ):
+        return items
+    smaller = [x for x in items[1:] if x[1] < items[0][1] ]
+    larger = [x for x in items[1:] if x[1] >= items[0][1] ]
+    return qSort(smaller) + [ items[0] ] + qSort(larger)
+    
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+    "*** YOUR CODE HERE ***" 
+    pricesList = []
+    for shop in fruitShops:
+        pricesList.insert(0, (shop, shop.getPriceOfOrder(orderList)) );
+    return qSort(pricesList)[0][0]
+
+if __name__ == '__main__':
+  "This code runs when you invoke the script from the command line"
+  orders = [('apples',1.0), ('oranges',3.0)]
+  dir1 = {'apples': 2.0, 'oranges':1.0}
+  shop1 =  shop.FruitShop('shop1',dir1)
+  dir2 = {'apples': 1.0, 'oranges': 5.0}
+  shop2 = shop.FruitShop('shop2',dir2)
+  shops = [shop1, shop2]
+  print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+  orders = [('apples',3.0)]
+  print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
